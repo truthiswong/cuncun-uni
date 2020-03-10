455a615c86b5f938871b46607e48d8e74b01a9ba
truthiswong <1457368987@qq.com>
2020-03-10 19:02:57 +0800

    登陆页面

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,37 +1,37 @@
 <template>
 	<view>
-		<iheader title="登录"></iheader>
-		<view class='base-padding'>
-			<form @submit="formSubmit">
-				<view class='form-body'>
-					<view>
-						<image src='/static/images/logo.png'></image>
-					</view>
-					<view class='row font-lv2'>
-						<view class='col-3'>账号</view>
-						<view class='col-9'>
-							<input name="username" auto-focus='true' placeholder="请输入用户名或邮箱" />
-						</view>
-					</view>
-					<view class='row font-lv2'>
-						<view class='col-3'>密码</view>
-						<view class='col-9'>
-							<input password name="password" placeholder="请输入密码" />
-						</view>
+		<view class="hint">
+			<image src="../../static/common/logo+ydb.png" mode=""></image>
+			<h3>您好，欢迎使用存存</h3>
+		</view>
+		<view style="padding: 50upx;">
+			<form @submit="login">
+				<view class="input_item">
+					<text class="input_text">手机号</text>
+					<input class="input_input" type="number" v-model="username" maxlength="11" placeholder-style="#CCCCCC" placeholder="请输入您的手机号码" />
+				</view>
+				<view class="input_item">
+					<text class="input_text">验证码</text>
+					<view class="row">
+						<input class="input_input col-6" type="number" v-model="sms" placeholder-style="#CCCCCC" placeholder="请输入短信验证码" />
+						<button :disabled="disabled" @click="getSms" class="sms_button col-6">{{smsText}}</button>
 					</view>
 				</view>
-				<view class='row mgb-30 font-lv3 color-grey'>
-					<navigator class='col' :url="'/pages/read/read?identify='+about">关于我们</navigator>
-					<view class='col text-right' @click='findPassword'>忘记密码？</view>
-				</view>
-				<view class='row'>
-					<button class='btn-submit btn-block' :loading='loading' form-type='submit'> 码上登录 </button>
-				</view>
-				<view class='row'>
-					<button @click="toReg" class='btn-login btn-block'> 注册账号 </button>
-				</view>
+				<label class="row" style="margin-top: 60upx;">
+					<view class="col-1" style="align-items: center;">
+						<checkbox style="transform:scale(0.6);" :checked="agreement" color="rgba(59, 193, 187, 1)"></checkbox>
+					</view>
+					<view class="row col-11" style="font-size: 26upx;color: #282828; align-items: center; margin-top: 8upx;">
+						<text class="col-2">我同意</text>
+						<navigator class="col-10" url="/pages/login/reg">
+							<text style="color: #0269D0;">《存存用户服务协议》</text>
+						</navigator>
+					</view>
+				</label>
+				<button form-type="submit" class="common_button">进去存存</button>
 			</form>
 		</view>
+		
 	</view>
 </template>
 
@@ -39,18 +39,19 @@
 	import config from '../../config.js'
 	import util from '../../utils/util.js'
 	import api from '../../utils/api.js'
-
-	import iheader from '../../components/header.vue'
-
 	export default {
-		components: {
-			iheader
-		},
+		components: {},
 		data() {
 			return {
+				inputValue: '',
 				loading: false,
-				about: config.info.about,
-				redirect: encodeURIComponent('/pages/me/me'),
+				username: "", //手机号
+				sms: "", //验证码
+				smsText: "发送验证码",
+				agreement: false,
+				buttonActive: false, // 颜色控制
+				disabled: false, // 是否可以点击
+				redirect: encodeURIComponent('/pages/tabs/tab1'),
 			}
 		},
 		onLoad: function(op) {
@@ -73,9 +74,49 @@
 			}
 		},
 		methods: {
+			onKeyInput: function(event) {
+				this.inputValue = event.target.value
+			},
 			toReg: function() {
 				uni.navigateTo({
 					url: '/pages/reg/reg?redirect=' + this.redirect
+				})
+			},
+			getSms() {
+				this.disabled = true;
+				let num = 30;
+				this.smsText = num;
+				this.timer = setInterval(() => {
+					num--;
+					if (num >= 0) {
+						this.smsText = num;
+					} else {
+						clearInterval(this.timer);
+						this.smsText = "再次获取";
+						this.disabled = false;
+					}
+				}, 1000);
+				// if (!this.username) {
+				// 	this.$notify({
+				// 		message: "请输入手机号",
+				// 		color: "#FFFFFF",
+				// 		background: "#E74243"
+				// 	});
+				// } else {
+				// 	this.$notify({
+				// 		type: "success",
+				// 		message: "验证码已发送"
+				// 	});
+
+				// }
+			},
+			login() {
+				// uni.navigateTo({
+				// 	url: '/pages/tabs/tab1'
+				// })
+				
+				uni.switchTab({
+					url: '/pages/tabs/tab1'
 				})
 			},
 			findPassword: function(e) {
@@ -130,14 +171,83 @@
 </script>
 
 <style scoped>
-	@import url("../../static/css/reg-login.css");
+	.hint {
+		padding: 50upx;
+	}
 
-	image {
-		width: 180upx;
-		height: 180upx;
-		border-radius: 90upx;
-		border: 1px solid #ddd;
-		margin: 15px auto;
+	.hint>image {
+		width: 374upx;
+		height: 48upx;
+	}
+
+	.hint>h3 {
+		font-size: 50upx;
+		font-weight: 500;
+		color: rgba(40, 40, 40, 1);
+		line-height: 70upx;
+		margin-top: 70upx;
+	}
+
+	.input_item {
+		margin-top: 20upx;
+		border-bottom: 2upx solid rgba(156, 168, 179, 1);
+	}
+
+	.input_text {
+		font-size: 24upx;
+		font-weight: 400;
+		color: rgba(74, 74, 74, 1);
+		line-height: 33upx;
+	}
+
+	.input_input {
+		font-size: 28upx;
+		font-weight: 400;
+		color: rgba(74, 74, 74, 1);
+		line-height: 33upx;
+		padding: 20upx 0;
+	}
+
+	.sms_button {
 		display: block;
+		width: 140upx;
+		font-size: 28upx;
+		font-weight: 400;
+		color: rgba(3, 166, 166, 1);
+		line-height: 80upx;
+		background-color: rgba(0, 0, 0, 0);
+		text-align: right;
+		padding: 0;
+	}
+
+	.sms_button:after {
+		border: 0 none;
+	}
+
+	button[disabled]:not([type]),
+	uni-button[disabled][type=default] {
+		color: rgba(0, 0, 0, .3);
+		background-color: rgba(0, 0, 0, 0);
+	}
+
+	.common_button {
+		width: 630upx;
+		height: 98upx;
+		background: rgba(238, 238, 238, 1);
+		border-radius: 50upx;
+		font-size: 30upx;
+		line-height: 98upx;
+		font-weight: 500;
+		color: rgba(178, 178, 178, 1);
+		margin: 80upx auto 0;
+	}
+
+	.common_button:after {
+		border: 0 none;
+	}
+
+	.common_button_active {
+		background: #3bc1bb;
+		color: white;
 	}
 </style>
