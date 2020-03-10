ffc858b8bb2f6071f7d267cbcd8ac6bde37b384f
truthiswong <1457368987@qq.com>
2020-03-10 19:02:37 +0800

    引入弹窗搜索列表

--- main.js
+++ main.js
@@ -3,11 +3,21 @@ import App from './App'
 
 Vue.config.productionTip = false
 
+
 // 导航栏全局注册
 import hxNavbar from "./components/hx-navbar/hx-navbar.vue"
 Vue.component('hx-navbar',hxNavbar)
-
-
+// 弹窗
+import uniPopup from "@/components/uni-popup/uni-popup.vue"
+Vue.component('uni-popup',uniPopup)
+// 搜索框
+import uniSearchBar from '@/components/uni-search-bar/uni-search-bar.vue'
+Vue.component('uni-searchBar',uniSearchBar)
+// 列表
+import uniList from "@/components/uni-list/uni-list.vue"
+import uniListItem from "@/components/uni-list-item/uni-list-item.vue"
+Vue.component('uni-list',uniList)
+Vue.component('uni-list-item',uniListItem)
 
 App.mpType = 'app'
 
