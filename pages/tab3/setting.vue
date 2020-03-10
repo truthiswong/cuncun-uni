09ec826a181caea1ad2edd889f3d952b1b458d1a
truthiswong <1457368987@qq.com>
2020-03-10 19:05:38 +0800

    设置

--- pages/tab3/setting.vue
+++ pages/tab3/setting.vue
@@ -1,34 +1,30 @@
 <template>
 	<div class="layout">
-		<hx-navbar :back="true" title="设置" color="rgba(40,40,40,1)" :fixed="true" barPlaceholder="hidden" transparent="auto"
+		<hx-navbar :back="true" title="设置" color="rgba(40,40,40,1)" barPlaceholder="hidden" transparent="auto"
 		 :background-color="[255, 255, 255]">
 			<view slot="left">
 				<image @click="onClickBack" src="../../static/images/header-back.png" style="width: 40upx; height: 40upx; margin: 30upx 32upx 0;"></image>
 			</view>
 		</hx-navbar>
 		<!-- 内容 -->
-		<div class="content">
-			<!-- <van-cell
-        center
-        class="head_image"
-        is-link
-        title="头像"
-      > -->
-			<!-- <img src="../../assets/img/tab3/my_image.png" alt /> -->
-			<!-- </van-cell>
-      <van-cell
-        is-link
-        title="昵称"
-        value="dinghan"
-      />
-      <van-cell
-        is-link
-        title="手机号"
-        value="13198279824"
-      />
-      <van-cell is-link @click="onAddress" title="地址管理" />
-      <van-cell is-link @click="onLogout" title="注销账号" /> -->
-		</div>
+		<view class="content">
+			<uni-list>
+				<uni-list-item title="头像" note="">
+					<!-- <image style="width: 120upx;height: 120upx;border-radius: 50%;margin-left: 300upx;" src="../../static/tab3/my_image.png" mode=""></image> -->
+				</uni-list-item>
+				<uni-list-item title="昵称" note=""></uni-list-item>
+				<uni-list-item title="手机号" note=""></uni-list-item>
+				<uni-list-item title="地址管理" note=""></uni-list-item>
+				<uni-list-item @click="onLogout" title="注册账号" note=""></uni-list-item>
+			</uni-list>
+			<!-- <van-cell center class="head_image" is-link title="头像">
+				<img src="../../static/tab3/my_image.png" alt />
+			</van-cell>
+			<van-cell is-link title="昵称" value="dinghan" />
+			<van-cell is-link title="手机号" value="13198279824" />
+			<van-cell is-link @click="onAddress" title="地址管理" />
+			<van-cell is-link @click="onLogout" title="注销账号" /> -->
+		</view>
 		<button @click="onExit" class="logout" block>退出账号</button>
 	</div>
 </template>
@@ -57,34 +53,26 @@
 				});
 			},
 			onLogout() {
-				this.$dialog
-					.confirm({
-						title: "提示",
-						message: "注销账号后，该账号里所有数据将被清空。"
-					})
-					.then(() => {
-						// on confirm
-
-					})
-					.catch(() => {
-						// on cancel
-					});
+				uni.showModal({
+					title: '提示',
+					content: '注销账号后，该账号里所有数据将被清空。',
+					success(res) {
+						if (res.confirm) {
+							uni.toastSuccess('退出成功')
+						}
+					}
+				})
 			},
 			onExit() {
-				let that = this
 				uni.showModal({
-					title: '温馨提示',
+					title: '提示',
 					content: '您确定要退出登录吗？',
 					success(res) {
 						if (res.confirm) {
-							util.request(config.api.logout) // 只需调用，不需要处理返回结果
-							util.clearUser()
-							util.toastSuccess('退出成功')
-							that.initUser()
-							util.setSignedAt(0)
-							let sysInfo = util.getSysInfo()
-							sysInfo.bookshelfChanged = true
-							util.setSysInfo(sysInfo)
+							uni.navigateTo({
+								url: '/pages/login/login'
+							})
+							uni.toastSuccess('退出成功')
 						}
 					}
 				})
@@ -121,6 +109,7 @@
 		box-shadow: 0 -20upx 10upx 0 rgba(0, 0, 0, 0.05);
 		font-size: 32upx;
 		line-height: 130upx;
+		border-radius: 16upx 16upx 0 0;
 		font-weight: 500;
 		color: rgba(255, 255, 255, 1);
 	}
