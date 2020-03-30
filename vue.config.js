e3ebedfbd02e0db838ea4092725de10c39824276
truthiswong <1457368987@qq.com>
2020-03-30 11:37:57 +0800

    vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+let host = 'http://cuncun.app.iisu.cn/server/sso'
+// vue.config.js
+const vueConfig = {
+  devServer: {
+    // development server port 8000
+    port: 8088,
+    // If you want to turn on the proxy, please remove the mockjs /src/main.jsL11
+    proxy: {
+      '/':{            //这里的key就是axios的baseURL
+        // target: 'http://demo-jleco-river.jl-shgroup.com/server/',    //访问域名http://www.qfii.club
+        target: host,    //访问域名http://www.qfii.club
+        changeOrigin: true,            //开启跨域
+		ws: false,
+        pathRewrite:{  // 路径重写，
+            '^/': ''  // 替换target中的请求地址
+        }
+      }
+    }
+  }
+}
+
+module.exports = vueConfig
