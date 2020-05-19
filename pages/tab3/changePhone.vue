159eb7409c93013e67dd0a1b7219213646e498a0
wong <1457368987@qq.com>
2020-05-19 16:35:26 +0800

    修改手机号

--- pages/tab3/changePhone.vue
+++ pages/tab3/changePhone.vue
@@ -4,14 +4,14 @@
 		<!-- 内容 -->
 		<view style="margin-top: 20upx;padding: 0 30upx;background-color: #FFFFFF;">
 			<uni-list class="list_custom list_custom_item list_custom_margin20">
-				<uni-list-item title="旧手机：" :showArrow="false">
+				<uni-list-item title="当前手机：" :showArrow="false">
 					<view slot="right" class="input">
-						<input type="number" v-model="userOld" maxlength="11" placeholder="请输入旧的手机号码" placeholder-style="color: #CCCCCC;font-size:14px;" />
+						<input type="text" v-model="userOld" disabled maxlength="11" placeholder="请输入旧的手机号码" placeholder-style="color: #CCCCCC;font-size:14px;" />
 					</view>
 				</uni-list-item>
 				<uni-list-item title="验证码：" :showArrow="false">
 					<view slot="right" class="sms row">
-						<input type="number" v-model="smsOld" class="col-8" placeholder="请输入旧手机验证码" placeholder-style="color: #CCCCCC;font-size:14px;" />
+						<input type="number" v-model="smsOld" maxlength="6" class="col-8" placeholder="请输入旧手机验证码" placeholder-style="color: #CCCCCC;font-size:14px;" />
 						<button :disabled="disabledOld" @click="getSmsOld" class="sms_button col-4">{{smsTextOld}}</button>
 					</view>
 				</uni-list-item>
@@ -22,13 +22,13 @@
 				</uni-list-item>
 				<uni-list-item title="验证码：" :showArrow="false">
 					<view slot="right" class="sms flex_between">
-						<input type="number" v-model="smsNew" class="col-8" placeholder="请输入旧手机验证码" placeholder-style="color: #CCCCCC;font-size:14px;" />
+						<input type="number" v-model="smsNew" maxlength="6" class="col-8" placeholder="请输入新手机验证码" placeholder-style="color: #CCCCCC;font-size:14px;" />
 						<button :disabled="disabledNew" @click="getSmsNew" class="sms_button col-4">{{smsTextNew}}</button>
 					</view>
 				</uni-list-item>
 			</uni-list>
 		</view>
-		<button type="default" class="address_button" :class="{address_button_active: buttonActive}">确定</button>
+		<button type="default" @click="onConfirm" class="address_button" :class="{address_button_active: buttonActive}">确定</button>
 	</view>
 </template>
 
@@ -47,6 +47,16 @@
 				disabledOld: false, // 是否可以点击
 				disabledNew: false, // 是否可以点击
 			};
+		},
+		onLoad() {
+			
+		},
+		onShow() {
+			let user = uni.getStorageSync('user')
+			console.log(user)
+			if (user.mobile) {
+				this.userOld = user.mobile
+			}
 		},
 		watch: {
 			userOld() {
@@ -91,19 +101,45 @@
 						icon: 'none'
 					})
 				} else {
-					this.disabledOld = true;
-					let num = 30;
-					this.smsTextOld = num;
-					this.timerOld = setInterval(() => {
-						num--;
-						if (num >= 0) {
-							this.smsTextOld = num;
-						} else {
-							clearInterval(this.timerOld);
-							this.smsTextOld = "再次获取";
-							this.disabledOld = false;
-						}
-					}, 1000);
+					uni.request({
+					    url: 'http://cuncun.app.iisu.cn/server/sso/code/send',
+					    data: {
+							type: 'mobile',
+							account: this.userOld
+						},
+						method: 'POST',
+					    header: {
+							'Content-Type': 'application/x-www-form-urlencoded',
+					        'X-TENANT-ID': 'cuncun:cc@2020'
+					    },
+					    success: (res) => {
+					        let data = res.data
+							if (data.success) {
+								uni.showToast({
+									icon: 'none',
+									title: data.message
+								});
+								this.disabledOld = true;
+								let num = 30;
+								this.smsTextOld = num;
+								this.timerOld = setInterval(() => {
+									num--;
+									if (num >= 0) {
+										this.smsTextOld = num;
+									} else {
+										clearInterval(this.timerOld);
+										this.smsTextOld = "再次获取";
+										this.disabledOld = false;
+									}
+								}, 1000);
+							} else {
+								uni.showToast({
+									icon: 'none',
+									title: data.message
+								});
+							}
+					    }
+					});
 				}
 			},
 			getSmsNew() {
@@ -113,19 +149,102 @@
 						icon: 'none'
 					})
 				} else {
-					this.disabledNew = true;
-					let num = 30;
-					this.smsTextNew = num;
-					this.timerNew = setInterval(() => {
-						num--;
-						if (num >= 0) {
-							this.smsTextNew = num;
-						} else {
-							clearInterval(this.timerNew);
-							this.smsTextNew = "再次获取";
-							this.disabledNew = false;
-						}
-					}, 1000);
+					uni.request({
+					    url: 'http://cuncun.app.iisu.cn/server/sso/code/send',
+					    data: {
+							type: 'mobile',
+							account: this.userNew
+						},
+						method: 'POST',
+					    header: {
+							'Content-Type': 'application/x-www-form-urlencoded',
+					        'X-TENANT-ID': 'cuncun:cc@2020'
+					    },
+					    success: (res) => {
+					        let data = res.data
+							if (data.success) {
+								uni.showToast({
+									icon: 'none',
+									title: data.message
+								});
+								this.disabledNew = true;
+								let num = 30;
+								this.smsTextNew = num;
+								this.timerNew = setInterval(() => {
+									num--;
+									if (num >= 0) {
+										this.smsTextNew = num;
+									} else {
+										clearInterval(this.timerNew);
+										this.smsTextNew = "再次获取";
+										this.disabledNew = false;
+									}
+								}, 1000);
+							} else {
+								uni.showToast({
+									icon: 'none',
+									title: data.message
+								});
+							}
+					    }
+					});
+				}
+			},
+			onConfirm() {
+				if (!this.userOld) {
+					uni.showToast({
+						title: '请输入旧的手机号码',
+						icon: 'none'
+					})
+				} else if (!this.smsOld) {
+					uni.showToast({
+						title: '请输入旧手机验证码',
+						icon: 'none'
+					})
+				} else if (!this.userNew) {
+					uni.showToast({
+						title: '请输入新的手机号码',
+						icon: 'none'
+					})
+				} else if (!this.smsNew) {
+					uni.showToast({
+						title: '请输入新手机验证码',
+						icon: 'none'
+					})
+				} else {
+					uni.request({
+					    url: 'http://cuncun.app.iisu.cn/server/sso/self/reset/mobile',
+					    data: {
+							oldCode: this.smsOld,
+							newMobile: this.userNew,
+							newCode: this.smsNew
+						},
+						method: 'POST',
+					    header: {
+							'Content-Type': 'application/x-www-form-urlencoded',
+					        'X-TENANT-ID': 'cuncun:cc@2020',
+							'Authorization': uni.getStorageSync('token')
+					    },
+					    success: (res) => {
+					        let data = res.data
+							if (data.success) {
+								uni.showToast({
+									icon: 'none',
+									title: '修改成功'
+								});
+								let pages = getCurrentPages(); //获取所有页面栈实例列表
+								let nowPage = pages[pages.length - 1]; //当前页页面实例
+								let prevPage = pages[pages.length - 2]; //上一页页面实例
+								prevPage.$vm.userInfoUpdate = true; //修改上一页data里面的couponNumber参数值为value
+								uni.navigateBack();
+							} else {
+								uni.showToast({
+									icon: 'none',
+									title: data.message
+								});
+							}
+					    }
+					});
 				}
 			}
 		}
