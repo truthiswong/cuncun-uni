41c59aa3576b3efa14f2917873c6d112b2f1a5b2
wong <1457368987@qq.com>
2020-05-20 18:22:46 +0800

    启动延迟500ms

--- pages/login/init.vue
+++ pages/login/init.vue
@@ -10,37 +10,38 @@
 			// 从本地缓存中同步获取指定 key 对应的内容，用于判断是否是第一次打开应用
 			this.$nextTick(()=>{
 				setTimeout(()=>{
+					// #ifdef APP-PLUS
 					plus.navigator.closeSplashscreen()
-				}, 3000)
-			})
-			try {
-				const value = uni.getStorageSync('launchFlag')
-				const token = uni.getStorageSync('token')
-				
-				if (value) {
-					if (token) {
-						this.$nextTick(()=>{
-							uni.switchTab({
-								url: '/pages/tabs/tab1'
-							})
-						})
-					} else {
-						uni.redirectTo({
-							url: '/pages/login/login'
-						});
+					// #endif
+					try {
+						const value = uni.getStorageSync('launchFlag')
+						const token = uni.getStorageSync('token')
+						if (value) {
+							if (token) {
+								this.$nextTick(()=>{
+									uni.switchTab({
+										url: '/pages/tabs/tab1'
+									})
+								})
+							} else {
+								uni.redirectTo({
+									url: '/pages/login/login'
+								});
+							}
+						} else {
+							uni.setStorage({
+								key: 'launchFlag',
+								data: true
+							});
+							uni.redirectTo({
+								url: '/pages/login/guide'
+							});
+						}
+					} catch (e) {
+						//TODO handle the exception
 					}
-				} else {
-					uni.setStorage({
-						key: 'launchFlag',
-						data: true
-					});
-					uni.redirectTo({
-						url: '/pages/login/guide'
-					});
-				}
-			} catch (e) {
-				//TODO handle the exception
-			}
+				}, 500)
+			})
 		}
 	}
 </script>
