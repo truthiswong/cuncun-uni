8d253b0c6857c52d6d8605bffeca1f8b04d67027
truthiswong <1457368987@qq.com>
2020-06-05 17:07:02 +0800

    下拉刷新

--- pages/tab3/payDetails.vue
+++ pages/tab3/payDetails.vue
@@ -1,7 +1,7 @@
 <template>
 	<view class="layout">
 		<uni-nav-bar left-icon="back" @clickLeft="onClickBack" title="消费明细" status-bar="true" fixed="true"></uni-nav-bar>
-		<uni-nav-bar fixed="true">
+		<!-- <uni-nav-bar fixed="true">
 			<view slot='left' style="width: 100upx;padding-left: 18upx;">
 				时间
 			</view>
@@ -13,20 +13,27 @@
 					消费项
 				</view>
 			</view>
-		</uni-nav-bar>
+		</uni-nav-bar> -->
 		<!-- 内容 -->
 		<view class="content">
-			<view v-for="(item,index) in list" :key="index" class="flex_between pay_list" style="width: 100%;">
-				<view style="width: 30%;">
-					<text>{{item.time}}</text>
-				</view>
-				<view style="width: 40%;text-align: center;padding-left: 2.5%;">
-					<text v-if="item.amount">¥ {{item.refund?'+':''}}{{item.amount}}</text>
-				</view>
-				<view style="width: 40%;text-align: right;">
-					<text>{{item.type.name}}</text>
-				</view>
-			</view>
+			<uni-list class="list_custom list_custom_align_start">
+				<uni-list-item v-for="(item,index) in list" :key="index" :showArrow="false">
+					<view slot="default" class="list_left">
+						<view class="list_left_icon">
+							<text class="list_left_text" :class="{'list_left_text_active': item.refund}">{{item.refund?'退':'支'}}</text>
+						</view>
+						<view class="list_left_title">
+							<text>{{item.subject}}</text>
+							<!-- <text>{{item.type.name}}</text> -->
+							<p>{{item.refundReason}}</p>
+							<p>{{item.time}}</p>
+						</view>
+					</view>
+					<view slot="right">
+						<text class="list_right" :class="{'list_right_active': item.refund}" v-if="item.amount">¥ {{item.refund?'+':'-'}}{{item.amount}}</text>
+					</view>
+				</uni-list-item>
+			</uni-list>
 			<view v-if="finished" style="text-align: center;font-size:24upx;font-weight:400;color:rgba(178,178,178,1);margin: 20upx 0;line-height:33upx;">
 				这是我的底线，没有更多的咯～
 			</view>
@@ -42,11 +49,17 @@
 				list: [],
 				pageNumber: 0,
 				totalPages: 1,
-				finished: false
+				finished: false,
 			};
 		},
 		onLoad() {},
 		onShow() {
+			this.getDetails()
+		},
+		onPullDownRefresh() {
+			this.pageNumber = 0
+			this.totalPages = 1
+			this.finished = false
 			this.getDetails()
 		},
 		onReachBottom() {
@@ -61,10 +74,16 @@
 			getDetails() {
 				if (this.totalPages > this.pageNumber) {
 					this.$http('user/payment/page?pageSize=20&pageNumber=' + this.pageNumber, "GET", '', res => {
+						// #ifdef APP-PLUS
+						uni.stopPullDownRefresh()
+						// #endif
 						let data = res.data
 						if (data.success) {
+							if (this.pageNumber == 0) {
+								this.list = []
+							}
 							for (let item of data.data.data) {
-								item.time = this.$moment(item.payTime).format('YYYY-MM-DD')
+								item.time = this.$moment(item.payTime).format('YYYY-MM-DD HH:mm:ss')
 								item.amount = item.amount.toFixed(2)
 							}
 							this.pageNumber++
@@ -99,15 +118,54 @@
 		overflow: hidden;
 	}
 
-	.pay_list {
-		box-sizing: border-box;
+	.content {
 		padding: 0 30upx;
-		width: 100%;
-		height: 94upx;
-		line-height: 94upx;
-		font-size: 28upx;
-		font-weight: 400;
-		color: rgba(40, 40, 40, 1);
-		border-bottom: 1upx solid rgba(236, 237, 238, .38);
+
+		.list_left {
+			display: flex;
+
+			.list_left_icon {
+				width: 60upx;
+				height: 60upx;
+				line-height: 60upx;
+				background-color: #EEEEEE;
+				border-radius: 50%;
+				text-align: center;
+				.list_left_text {
+					display: block;
+					width: 60upx;
+					height: 60upx;
+					line-height: 60upx;
+					font-size: 28upx;
+					color: #333333;
+				}
+				.list_left_text_active {
+					color: #03A6A6;
+				}
+			}
+
+			.list_left_title {
+				box-sizing: border-box;
+				padding: 0 20upx;
+				text {
+					font-size: 28upx;
+				}
+
+				p {
+					font-size: 24upx;
+					margin-top: 12upx;
+					color: rgba(136, 136, 136, 1);
+				}
+			}
+		}
+
+		.list_right {
+			color: #333333;
+			font-size: 36upx;
+			font-weight: 500;
+		}
+		.list_right_active {
+			color: #DF5000;
+		}
 	}
 </style>
